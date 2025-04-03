f6c0f33798bee6f6bbd5034f7ea9850d31607327
Pablo22852 <pferher1106@g.educaand.es>
2025-04-03 12:43:18 +0200

    actualizacion

--- EjerEjemp/script.js
+++ EjerEjemp/script.js
@@ -31,14 +31,50 @@ profileImage.addEventListener('mouseout', () => {
 //     // }
 // });
 
-const nameColorButton = document.getElementById("changeColorName");
-nameColorButton.addEventListener('click', () => {
-    const fullNameH1 = document.getElementById('fullName');
-    if (fullNameH1.style.color == '') {
-        // fullNameH1.style.color = 'blue';
-        fullNameH1.style= "color: green; font-style: italic;";
-    } else {
-        fullNameH1.style.color = '';
-    }
+// const nameColorButton = document.getElementById("changeColorName");
+// const Name = titleName.textContent;
+    // const fullNameH1 = document.getElementById('fullName');
 
-});
+// nameColorButton.addEventListener('click', () => {
+//     if (fullNameH1.style.color == '') {
+//         // fullNameH1.style.color = 'blue';
+//         fullNameH1.style= "color: green; font-style: italic;";
+//         fullNameH1.textContent= '1ºDAW';
+//         // titleName.innerHTML = "</br></br></br>1º DAW";
+
+//     } else {
+//         // fullNameH1.style.color = '';
+//         fullNameH1.style= '';
+//         fullNameH1.textContent= 'Pablo';
+//         // titleName.style.color = '';
+//         // titleName.innerHTML = "Pablo";
+
+//     }
+
+// });
+
+// const showFormButton = document.getElementById('showFormButton');
+// const contactForm = document.getElementById('contactForm');
+
+//     showFormButton.addEventListener('click', () => {
+//     if (contactForm.style.display === 'none') {
+//         contactForm.style.display = 'block';
+//         showFormButton.textContent = "Hide Form";
+//     } else {
+//         contactForm.style.display = 'none';
+//         showFormButton.textContent = "Contact Me";
+//     }
+// });
+
+    const OcultarNameButton = document.getElementById('changeColorName');
+    OcultarNameButton.addEventListener('click', () => {
+    console.warn('click-');
+    if (titleName.style.display === "block") {
+        titleName.style.display = "none";
+    } else {
+        titleName.style.display = "block";
+    }
+    });
+
+
+    
